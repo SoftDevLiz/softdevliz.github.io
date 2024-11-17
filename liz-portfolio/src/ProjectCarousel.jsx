02abf5a1afd2a3b0712edf93323f52e1b43b89f3
Liz Mouton <liz.mouton1@gmail.com>
2024-11-17 12:29:46 +0200

    Add carousel images + rename carousel component

---
+++ liz-portfolio/src/ProjectCarousel.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Carousel } from 'react-responsive-3d-carousel';
+import 'react-responsive-3d-carousel/dist/styles.css'; 
+import p1 from "./assets/Project 1.png"
+import p2 from "./assets/Project 1-2.png"
+import p3 from "./assets/Project 1-3.png"
+
+const items = [
+  <img src={p1} alt="Project 1" />,
+  <img src={p2} alt="Project 2" />,
+  <img src={p3} alt="Project 3" />,
+];
+
+function ProjectCarousel() {
+  return (
+    <div className="App">
+      <Carousel
+        items={items}
+        startIndex={0}
+        onChange={(currentIndex) => console.log(currentIndex)}
+      />
+    </div>
+  );
+}
+
+export default ProjectCarousel;
--- liz-portfolio/src/main.jsx
+++ liz-portfolio/src/main.jsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import Carousel from './ProjectCarousel.jsx'
 import './index.css'
-import App from './App.jsx'
+import ProjectCarousel from './ProjectCarousel.jsx'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ProjectCarousel/>
   </StrictMode>,
 )
