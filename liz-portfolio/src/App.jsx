0bdbde777f752f600a66efbece451e18c42a5995
Liz Mouton <liz.mouton1@gmail.com>
2024-11-22 15:10:25 +0200

    Set up MUI timeline component

--- liz-portfolio/src/App.jsx
+++ liz-portfolio/src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Hero from "./components/Hero";
+import MyTimeline from "./components/MyTimeline";
 import ProjectCarousel from "./components/ProjectCarousel";
 
 function App() {
   return (
     <>
       <Hero />
+      <MyTimeline />
       <ProjectCarousel />
     </>
   )
---
+++ liz-portfolio/src/components/MyTimeline.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import Timeline from '@mui/lab/Timeline';
+import TimelineItem from '@mui/lab/TimelineItem';
+import TimelineSeparator from '@mui/lab/TimelineSeparator';
+import TimelineConnector from '@mui/lab/TimelineConnector';
+import TimelineContent from '@mui/lab/TimelineContent';
+import TimelineDot from '@mui/lab/TimelineDot';
+
+function MyTimeline() {
+    return (
+        <div>
+                <Timeline position="alternate">
+      <TimelineItem>
+        <TimelineSeparator>
+          <TimelineDot />
+          <TimelineConnector />
+        </TimelineSeparator>
+        <TimelineContent>Eat</TimelineContent>
+      </TimelineItem>
+      <TimelineItem>
+        <TimelineSeparator>
+          <TimelineDot />
+          <TimelineConnector />
+        </TimelineSeparator>
+        <TimelineContent>Code</TimelineContent>
+      </TimelineItem>
+      <TimelineItem>
+        <TimelineSeparator>
+          <TimelineDot />
+          <TimelineConnector />
+        </TimelineSeparator>
+        <TimelineContent>Sleep</TimelineContent>
+      </TimelineItem>
+      <TimelineItem>
+        <TimelineSeparator>
+          <TimelineDot />
+        </TimelineSeparator>
+        <TimelineContent>Repeat</TimelineContent>
+      </TimelineItem>
+    </Timeline>
+        </div>
+    )
+}
+
+export default MyTimeline;
+
