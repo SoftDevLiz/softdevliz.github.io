06a3f5dd7577bd1b9cccd9f9fc32b3561d10b9b3
Liz Mouton <liz.mouton1@gmail.com>
2024-11-23 18:51:28 +0200

    Add body styling

--- liz-portfolio/src/App.jsx
+++ liz-portfolio/src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Hero from "./components/Hero";
 import MyTimeline from "./components/MyTimeline";
 import ProjectCarousel from "./components/ProjectCarousel";
+import "./styles/stylesheet.css"
 
 function App() {
   return (
