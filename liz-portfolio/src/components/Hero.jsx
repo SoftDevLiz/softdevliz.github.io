f16a587d7d4c4d5566e753f137cd975733ec01fc
Liz Mouton <liz.mouton1@gmail.com>
2024-11-23 19:00:56 +0200

    Add hover styling to hero

--- liz-portfolio/src/components/Hero.jsx
+++ liz-portfolio/src/components/Hero.jsx
@@ -4,7 +4,13 @@ import "../styles/stylesheet.css"
 function Hero() {
     return (
         <div className="hero">
-            <h1>Liezl Mouton_</h1>
+            <h1>
+                {Array.from("Liezl Mouton_").map((char, index) => (
+                <span key={index}>
+                {char === " " ? "\u00A0" : char}
+                </span>
+                ))}   
+            </h1>
             <h2>Junior Developer</h2>
         </div>
     )
