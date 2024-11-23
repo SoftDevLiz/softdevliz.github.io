479d30b85e26ee41d25357b139afc6bfbdc57707
Liz Mouton <liz.mouton1@gmail.com>
2024-11-23 20:29:23 +0200

    Style + animate underscore

--- liz-portfolio/src/components/Hero.jsx
+++ liz-portfolio/src/components/Hero.jsx
@@ -4,13 +4,7 @@ import "../styles/stylesheet.css"
 function Hero() {
     return (
         <div className="hero">
-            <h1>
-                {Array.from("Liezl Mouton_").map((char, index) => (
-                <span key={index}>
-                {char === " " ? "\u00A0" : char}
-                </span>
-                ))}   
-            </h1>
+            <h1>Liezl Mouton<span className="underscore">_</span></h1>
             <h2>Junior Developer</h2>
         </div>
     )
