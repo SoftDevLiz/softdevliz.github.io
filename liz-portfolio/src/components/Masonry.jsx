15fe3ba55f109868c6ac4cec2cd2e11684d8e7d1
Liz Mouton <liz.mouton1@gmail.com>
2024-11-24 21:21:16 +0200

    Pull in MUI masonry component

--- liz-portfolio/src/components/Masonry.jsx
+++ liz-portfolio/src/components/Masonry.jsx
@@ -1,12 +1,25 @@
-import React from "react";
-import Pincture from "../assets/codespace.jpeg"
+import React from 'react';
+import Box from '@mui/material/Box';
+import ImageList from '@mui/material/ImageList';
+import ImageListItem from '@mui/material/ImageListItem';
 
 function Masonry() {
-    return (
-        <div>
-            <img src={Pincture} width={"100px"} />
-        </div>
-    )
+  return (
+    <Box sx={{ width: 500, height: 450, overflowY: 'scroll' }}>
+      <ImageList variant="masonry" cols={3} gap={8}>
+        {itemData.map((item) => (
+          <ImageListItem key={item.img}>
+            <img
+              srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
+              src={`${item.img}?w=248&fit=crop&auto=format`}
+              alt={item.title}
+              loading="lazy"
+            />
+          </ImageListItem>
+        ))}
+      </ImageList>
+    </Box>
+  );
 }
 
 export default Masonry;
