b492b7eababffaecf5403cb42d8669f98d6ab4e6
Liz Mouton <liz.mouton1@gmail.com>
2024-11-26 18:35:35 +0200

    Add assets into pub folder for masonry wall

--- liz-portfolio/src/components/Masonry.jsx
+++ liz-portfolio/src/components/Masonry.jsx
@@ -1,24 +1,51 @@
 import React from 'react';
+import Box from '@mui/material/Box'
 import ImageList from '@mui/material/ImageList';
 import ImageListItem from '@mui/material/ImageListItem';
 
 const itemData = [
   {
-    img2: '../assets/code2.jpeg',
+    img: '/assets/code2.jpeg',
     title: 'Setup'
   },
   {
-    img: '../assets/coding.jpg',
+    img: '/assets/coding.jpg',
     title: 'Coding'
   },
   {
-    img: '../assets/me3.jpg',
+    img: '/assets/me3.jpg',
     title: 'Me'
+  },
+  {
+    img: '/assets/bike.mp4',
+    title: 'Bike'
+  },
+  {
+    img: '/assets/me1.jpeg',
+    title: 'Me'
+  },
+  {
+    img: '/assets/me2.jpg',
+    title: 'Me'
+  },
+  {
+    img: '/assets/meixdf.jpg',
+    title: 'Me Ixdf'
+  },
+  {
+    img: '/assets/mevid.mp4',
+    title: 'Me'
+  },
+  {
+    img: '/assets/setup.jpg',
+    title: 'Setup'
   }
+
 ]
 
 function Masonry() {
   return (
+    <Box sx={{ width: 500, height: 450, overflowY: 'scroll' }}>
       <ImageList variant="masonry" cols={3} gap={8}>
         {itemData.map((item) => (
           <ImageListItem key={item.img}>
@@ -30,6 +57,7 @@ function Masonry() {
           </ImageListItem>
         ))}
       </ImageList>
+      </Box>
   );
 }
 
