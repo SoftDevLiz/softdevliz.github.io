a415f4ce315a5973238c7e8e6977d822e06ca984
Liz Mouton <liz.mouton1@gmail.com>
2024-12-14 14:45:47 +0200

    Refactor AboutMeHEading, getting rid of extra div causing confusion

--- liz-portfolio/src/components/AboutMeHeading.jsx
+++ liz-portfolio/src/components/AboutMeHeading.jsx
@@ -3,11 +3,9 @@ import Arrow from "../assets/arrow-left.png"
 
 function AboutMeHeading() {
     return (
-        <div>
-            <div className="aboutme-heading">
-                <h2>Me</h2> 
-                <img src={Arrow}/>
-            </div>
+        <div className="aboutme-heading">
+            <h2>Me</h2> 
+            <img src={Arrow}/>
         </div>
     )
 }
